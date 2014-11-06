a4701fb252b9c21ec1f7550a050a818bd5008a9c
colenhyt <colen.hyt@gmail.com>
2014-11-06 17:36:06 +0800

    delete some files

--- static/js/toplist.js
+++ static/js/toplist.js
@@ -1,4 +1,23 @@
 
+function addRow(obj)
+{
+	//alert('ddd addrow');
+	//添加一行
+	var tab = document.getElementById(obj);
+	for (var i=0;i<10 ; i++)
+	{
+	var newTr = tab.insertRow(i+1);
+	//添加列
+	var newTd0 = newTr.insertCell();
+	var newTd1 = newTr.insertCell();
+	var newTd2 = newTr.insertCell();
+	//设置列内容和属性
+	newTd0.innerHTML = '9999'; 
+	newTd1.innerText= '孙小雨';
+	newTd2.innerText= i;
+	}
+}
+
 function getdata(args) {
     //code
 }
