b0f7992c46dc1bef428f251186fd985b896f84b5
colenhyt <colen.hyt@gmail.com>
2015-11-03 17:51:23 +0800

    分布式

--- src/cn/hd/mgr/DataManager.java
+++ src/cn/hd/mgr/DataManager.java
@@ -28,43 +28,47 @@ import cn.hd.util.FileUtil;
 
 import com.alibaba.fastjson.JSON;
 
-public class DataManager extends MgrBase{
-	protected Logger  log = Logger.getLogger(getClass()); 
-	
+public class DataManager extends MgrBase {
+	protected Logger log = Logger.getLogger(getClass());
+
 	private LoginAction loginAction;
-	private Init	init;
+	private Init init;
 	public Config cfg;
+
 	public Init getInit() {
 		return init;
 	}
 
-	public Map<String,Integer> playerIdMaps;
-	public Map<Integer,PlayerWithBLOBs> playerMaps;
+	public Map<String, Integer> playerIdMaps;
+	public Map<Integer, PlayerWithBLOBs> playerMaps;
 	private int nextPlayerId;
 	private DataThread dataThread;
-	
-    private static DataManager uniqueInstance = null;  
-	
-    public static DataManager getInstance() {  
-        if (uniqueInstance == null) {  
-            uniqueInstance = new DataManager();  
-        }  
-        return uniqueInstance;  
-     } 
-    
-    private void initStock(PlayerWithBLOBs player){
-    	Connection conn = null;
+
+	private static DataManager uniqueInstance = null;
+
+	public static DataManager getInstance() {
+		if (uniqueInstance == null) {
+			uniqueInstance = new DataManager();
+		}
+		return uniqueInstance;
+	}
+
+	private void initStock(PlayerWithBLOBs player) {
+		Connection conn = null;
 		Statement stat = null;
 		try {
-			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/hdcf","root","123a123@");
+			conn = DriverManager.getConnection(
+					"jdbc:mysql://localhost:3306/hdcf", "root", "123a123@");
 			stat = conn.createStatement();
-	         ResultSet rs = stat.executeQuery("SELECT amount FROM stock where playerid="+player.getPlayerid());  
-	         while (rs.next()){  
-	             float mm = rs.getFloat(1); 
-//	             System.out.println(mm);
-	         } 	
-	         rs.close();
-	    } catch (SQLException e) {
+			ResultSet rs = stat
+					.executeQuery("SELECT amount FROM stock where playerid="
+							+ player.getPlayerid());
+			while (rs.next()) {
+				float mm = rs.getFloat(1);
+				// System.out.println(mm);
+			}
+			rs.close();
+		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} finally {
@@ -75,12 +79,12 @@ public class DataManager extends MgrBase{
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-		}    	
-    }
-    
-    public DataManager(){
-    	nextPlayerId = 0;
-    	try {
+		}
+	}
+
+	public DataManager() {
+		nextPlayerId = 0;
+		try {
 			Class.forName("com.mysql.jdbc.Driver").newInstance();
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
@@ -92,14 +96,14 @@ public class DataManager extends MgrBase{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-     }
-	
-    public synchronized int assignNextId(){
+	}
+
+	public synchronized int assignNextId() {
 		nextPlayerId++;
 		return nextPlayerId;
 	}
-	
-	public synchronized String login(String playerName,String tel,String sex){
+
+	public synchronized String login(String playerName, String tel, String sex) {
 		PlayerWithBLOBs pp = new PlayerWithBLOBs();
 		pp.setPlayername(playerName);
 		pp.setTel(tel);
@@ -107,74 +111,82 @@ public class DataManager extends MgrBase{
 		loginAction.setPlayer(pp);
 		return loginAction.login();
 	}
-	
-	public synchronized String getData(int playerid,int typeid){
+
+	public synchronized String getData(int playerid, int typeid) {
 		String data = "";
-		if (typeid==1){
-		 data = get_saving(playerid);
-		}else if (typeid==2){
+		if (typeid == 1) {
+			data = get_saving(playerid);
+		} else if (typeid == 2) {
 			data = get_insure(playerid);
-		}else if (typeid==3){
+		} else if (typeid == 3) {
 			data = get_stock(playerid);
-		}else if (typeid==4){
-		int top = get_top(playerid);
-		data = String.valueOf(top);
+		} else if (typeid == 4) {
+			int top = get_top(playerid);
+			data = String.valueOf(top);
 		}
 		return data;
 	}
 
-	public synchronized String get_saving(int playerid){
-		Map<Integer,Saving> data = loginAction.findUpdatedSavings(playerid);
+	public synchronized String get_saving(int playerid) {
+		Map<Integer, Saving> data = loginAction.findUpdatedSavings(playerid);
 		return JSON.toJSONString(data);
 	}
-	
-	public synchronized String get_insure(int playerid){
-		Map<Integer,Insure> data = loginAction.findUpdatedInsures(playerid);
+
+	public synchronized String get_insure(int playerid) {
+		Map<Integer, Insure> data = loginAction.findUpdatedInsures(playerid);
 		return JSON.toJSONString(data);
 	}
-	
-	public synchronized String get_stock(int playerid){
-		Map<Integer,List<Stock>> data = StockManager.getInstance().findMapStocks(playerid);
+
+	public synchronized String get_stock(int playerid) {
+		Map<Integer, List<Stock>> data = StockManager.getInstance()
+				.findMapStocks(playerid);
 		return JSON.toJSONString(data);
 	}
-	
-	public synchronized int get_top(int playerid){
+
+	public synchronized int get_top(int playerid) {
 		float fMm = loginAction.calculatePlayerMoney(playerid);
 
-		int top = ToplistManager.getInstance().findCountByGreaterMoney(playerid,0,fMm);
-		//800ms/1k
-		return top+1;
+		int top = ToplistManager.getInstance().findCountByGreaterMoney(
+				playerid, 0, fMm);
+		// 800ms/1k
+		return top + 1;
 	}
-	
-	
-	public synchronized boolean addPlayer(PlayerWithBLOBs player){
+
+	public synchronized boolean addPlayer(PlayerWithBLOBs player) {
 		if (playerMaps.containsKey(player.getPlayerid()))
 			return false;
-		
+
 		playerMaps.put(player.getPlayerid(), player);
 		playerIdMaps.put(player.getPlayername(), player.getPlayerid());
 		dataThread.push(player);
-//		newPlayersVect.add(player);
+		// newPlayersVect.add(player);
 		return true;
 	}
-	
-	public synchronized PlayerWithBLOBs findPlayer(String playerName){
+
+	public synchronized PlayerWithBLOBs findPlayer(String playerName) {
 		Integer playerid = playerIdMaps.get(playerName);
-		if (playerid!=null){
-			return playerMaps.get(playerid);
+		if (playerid == null) {
+			PlayerService playerService = new PlayerService();
+			PlayerWithBLOBs player = playerService.findByName(playerName);
+			if (player!=null){
+				playerid = player.getPlayerid();
+				playerMaps.put(player.getPlayerid(), player);
+				playerIdMaps.put(player.getPlayername(), player.getPlayerid());				
+			}
 		}
-		return null;
+		return findPlayer(playerid);
 	}
-	
-	public synchronized PlayerWithBLOBs findPlayer(int playerid){
-		return playerMaps.get(playerid);
+
+	public synchronized PlayerWithBLOBs findPlayer(int playerid) {
+		PlayerWithBLOBs player = playerMaps.get(playerid);
+		return player;
 	}
-	
-	public synchronized boolean updatePlayer(PlayerWithBLOBs player){
+
+	public synchronized boolean updatePlayer(PlayerWithBLOBs player) {
 		PlayerWithBLOBs pp = playerMaps.get(player.getPlayerid());
-		if (pp!=null){
+		if (pp != null) {
 			pp.setExp(player.getExp());
-			
+
 			PlayerWithBLOBs p = new PlayerWithBLOBs();
 			p.setPlayerid(pp.getPlayerid());
 			p.setExp(pp.getExp());
@@ -183,76 +195,88 @@ public class DataManager extends MgrBase{
 		}
 		return false;
 	}
-	
-	public synchronized void addSignin(int playerid){
+
+	public synchronized void addSignin(int playerid) {
 		dataThread.addSignin(playerid);
 	}
-	
-	public synchronized void addDoneQuest(int playerid){
+
+	public synchronized void addDoneQuest(int playerid) {
 		dataThread.addDoneQuest(playerid);
 	}
-	
-	public synchronized boolean updateZan(int playerid,int zan){
+
+	public synchronized boolean updateZan(int playerid, int zan) {
 		PlayerWithBLOBs pp = findPlayer(playerid);
-		if (pp!=null){
+		if (pp != null) {
 			pp.setZan(zan);
 			dataThread.updatePlayer(pp);
 			return true;
 		}
 		return false;
 	}
-	
-	public void init(){
-		String path = Thread.currentThread().getContextClassLoader().getResource("/").getPath();
-		String cfgstr = FileUtil.readFile(path+"config.properties");
-		if (cfgstr==null||cfgstr.trim().length()<=0){
-			return;
-		}
-		JSONObject ppObj = JSONObject.fromObject(cfgstr);
-		cfg = (Config)JSONObject.toBean(ppObj,Config.class);
-		System.out.println(cfgstr);
-		
-    	loginAction = new LoginAction();
-    	
-    	InitdataService initdataService = new InitdataService();
-    	init = initdataService.findInit();
-    	
-    	dataThread = new DataThread();
-    	dataThread.start();    	
-		
-    	playerIdMaps = Collections.synchronizedMap(new HashMap<String,Integer>());
-    	playerMaps = Collections.synchronizedMap(new HashMap<Integer,PlayerWithBLOBs>());
-    	PlayerService playerService = new PlayerService();
+
+	public synchronized void load() {
+		playerIdMaps.clear();
+		playerMaps.clear();
+		nextPlayerId = 0;
+		PlayerService playerService = new PlayerService();
 		List<PlayerWithBLOBs> players = playerService.findAll();
-		for (int i=0; i<players.size();i++){
+		for (int i = 0; i < players.size(); i++) {
 			PlayerWithBLOBs player = players.get(i);
 			playerMaps.put(player.getPlayerid(), player);
 			playerIdMaps.put(player.getPlayername(), player.getPlayerid());
-			if (player.getPlayerid()>nextPlayerId)
-				nextPlayerId = player.getPlayerid();			
+			if (player.getPlayerid() > nextPlayerId)
+				nextPlayerId = player.getPlayerid();
 		}
-		System.out.println("load all players :"+players.size());
-		
-    }
-    public static void main(String[] args) {
-    	DataManager stmgr = DataManager.getInstance();
-    	SavingManager.getInstance().init();
+		System.out.println("load all players :" + players.size());
+
+	}
+
+	public void init() {
+		String path = Thread.currentThread().getContextClassLoader()
+				.getResource("/").getPath();
+		String cfgstr = FileUtil.readFile(path + "config.properties");
+		if (cfgstr == null || cfgstr.trim().length() <= 0) {
+			return;
+		}
+		JSONObject ppObj = JSONObject.fromObject(cfgstr);
+		cfg = (Config) JSONObject.toBean(ppObj, Config.class);
+		System.out.println(cfgstr);
+
+		loginAction = new LoginAction();
+
+		InitdataService initdataService = new InitdataService();
+		init = initdataService.findInit();
+
+		dataThread = new DataThread();
+		// dataThread.start();
+
+		playerIdMaps = Collections
+				.synchronizedMap(new HashMap<String, Integer>());
+		playerMaps = Collections
+				.synchronizedMap(new HashMap<Integer, PlayerWithBLOBs>());
+		this.load();
+	}
+
+	public static void main(String[] args) {
+		DataManager stmgr = DataManager.getInstance();
+		SavingManager.getInstance().init();
 		InsureManager.getInstance().init();
 		StockManager.getInstance().init();
-		ToplistManager.getInstance().init();    	
-    	stmgr.init();
-    	float count = 50000;
-    	long s = System.currentTimeMillis();
-    	for (int i=0;i<count;i++){
-    		String s2 = String.valueOf(i);
-    		String str = stmgr.login(s2, s2, "1");
-//    		System.out.println(str);
-    	}
-    	float e = System.currentTimeMillis()-s;
-    	System.out.println("run "+count+", cost time : "+e+"ms,"+(e/count)+"s/1000");
-//    	SavingdataService ss = new SavingdataService();
-//    	Savingdata dd = ss.findActive();
-//    	DataThread aa = new DataThread();
-//    	aa.start();
-    }
+		ToplistManager.getInstance().init();
+		stmgr.init();
+		float count = 50000;
+		long s = System.currentTimeMillis();
+		for (int i = 0; i < count; i++) {
+			String s2 = String.valueOf(i);
+			String str = stmgr.login(s2, s2, "1");
+			// System.out.println(str);
+		}
+		float e = System.currentTimeMillis() - s;
+		System.out.println("run " + count + ", cost time : " + e + "ms,"
+				+ (e / count) + "s/1000");
+		// SavingdataService ss = new SavingdataService();
+		// Savingdata dd = ss.findActive();
+		// DataThread aa = new DataThread();
+		// aa.start();
+	}
 }
